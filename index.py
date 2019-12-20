125a652d1f88b0088aaee0cf8389639b0cc5288d
Mateen Ahmad <mateenahmad49957@gmail.com>
2019-12-20 23:47:29 +0500

    Finalize

--- index.py
+++ index.py
@@ -2,58 +2,45 @@ import random
 import pandas as pd
 import sys
 
-data = pd.read_csv(sys.argv[2])
-dataList = data.values.tolist()
-numberOfColumns = len(data.columns)
-numberOfRows = len(dataList)
-thetha = 0.9
+
+threshold = 0.2
 
 
 def learning():
-    # Or gate Learning
-
-    # in and gate you can increase decimal points as much as you want
-    # to round the out to 1 decimal point
-    # weight1 = round(random.uniform(-0.5, 0.5), 1)
-    # to round the out to 1 decimal point
-    # weight2 = round(random.uniform(-0.5, 0.5), 1)
-    alpha = 0.1
-    flage = True     # flage will be false when convergence reaches
-    #output = 1
+    data = pd.read_csv(sys.argv[2])
+    #data = data.iloc[:100]
+    dataList = data.values.tolist()
+    numberOfColumns = len(data.columns)
+    numberOfRows = len(dataList)
+    #print("Rows: ", numberOfRows)
+    learningRate = 0.1
+    flage = True
     weights = []
     for i in range(0, numberOfColumns-1):
         weights.append(round(random.uniform(-0.5, 0.5), 3))
 
-    epoch = 0
     numberOfInputs = len(weights)
-    log = open('log.txt', 'w')
     while(flage == True):
-        epoch += 1
-        log.write("Epoch: ")
-        log.write('%d\n' % epoch)
-
         flage = False
         for row in dataList:
             value = 0
             for i in range(0, numberOfInputs):
                 value = value + (row[i]*weights[i])
-            if(value >= thetha):  # if perceptoron excited
+            if(value >= threshold):  # if perceptoron excited
                 output = 1
             else:
                 output = 0
 
-            log.write("Epoch: ")
             index = numberOfColumns-1
             if(output != row[index]):
                 for i in range(0, numberOfColumns-1):
                     weights[i] = round(
-                        weights[i]+(alpha*(row[index]-output)*row[i]), 3)
+                        weights[i]+(learningRate*(row[index]-output)*row[i]), 3)
                 # to round the out to 1 decimal point
-                print(weights)
+                # print(weights)
                 flage = True
 
-    print(weights)
-    log.close()
+    # print(weights)
 
     weightsFile = open('weights.txt', 'w')
     for i in range(0, numberOfInputs):
@@ -62,6 +49,12 @@ def learning():
 
 
 def testing():
+    data = pd.read_csv(sys.argv[2])
+    #data = data.iloc[300:]
+    dataList = data.values.tolist()
+    numberOfColumns = len(data.columns)
+    numberOfRows = len(dataList)
+    #print("Rows: ", numberOfRows)
     weights = []
     weightsFile = open('weights.txt', 'r')
     for w in weightsFile.readlines():
@@ -81,8 +74,7 @@ def testing():
         actual = 0
         for k in range(0, numberOfInputs):
             actual += weights[k]*dataList[i][k]
-        # desired = weights[0]*listt[i][0] + weights[1]*listt[i][1]
-        if actual < thetha:
+        if actual < threshold:
             actual = 0
         else:
             actual = 1
@@ -94,27 +86,25 @@ def testing():
     fp = 0
     fn = 0
     for i in range(0, numberOfRows):
-        if (out.iloc[i, -1] == out.iloc[i, -2] & out.iloc[i, -1] == 1):
+        if (out.iloc[i, -1] == out.iloc[i, -2]) & (out.iloc[i, -1] == 1):
             tp += 1
-        elif(out.iloc[i, -1] == out.iloc[i, -2] & out.iloc[i, -1] == 0):
+        elif(out.iloc[i, -1] == out.iloc[i, -2]) & (out.iloc[i, -1] == 0):
             tn += 1
-        elif(out.iloc[i, -1] != out.iloc[i, -2] & out.iloc[i, -1] == 1):
+        elif(out.iloc[i, -1] != out.iloc[i, -2]) & (out.iloc[i, -1] == 1):
             fp += 1
-        elif(out.iloc[i, -1] != out.iloc[i, -2] & out.iloc[i, -1] == 0):
+        elif(out.iloc[i, -1] != out.iloc[i, -2]) & (out.iloc[i, -1] == 0):
             fn += 1
 
     precision = tp/(tp+fp)*100
     recall = tp/(tp+fn)*100
     accuracy = (tp+tn)/(tp+tn+fp+fn)*100
-    results = open('results.txt', 'w')
+    results = open('Accuracy.txt', 'w')
     results.write("Precision: ")
     results.write('%d\n' % precision)
     results.write("Recall: ")
     results.write('%d\n' % recall)
     results.write("Accuracy: ")
     results.write('%d\n' % accuracy)
-    # results.write("recall: ", str(recall))
-    # results.write("accuracy: ", str(accuracy))
     results.close()
 
     print(precision)
